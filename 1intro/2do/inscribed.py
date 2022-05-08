9556dd65be2774178b1bb4b609e930df70dacbe7
jrgpulido <jrgpulido@gmail.com>
2022-05-07 23:25:52 -0500

    exercise n file updating

--- 1intro/2do/inscribed.py
+++ 1intro/2do/inscribed.py
@@ -1,4 +1,6 @@
 #
-# radious of circle inscribed in a tringle
+# radious of circle inscribed in a triangle
 # place here your solution code
 #
+# zeeAlso
+# https://keisan.casio.com/exec/system/1223428152
---
+++ 2fundamentals/2do/stats.py
@@ -0,0 +1,18 @@
+#
+# select and write 
+# one of the following stat functions
+#
+
+def sum(lst): pass
+
+def avg(l): pass
+
+def min(l): pass
+
+def max(l): pass
+
+def range(l): pass
+
+def std(l): pass
+
+def mode(l): pass
