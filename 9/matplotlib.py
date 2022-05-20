758a76107ea22b615ea71c42914ea2a4fcac00b3
jrgpulido <jrgpulido@gmail.com>
2022-05-20 07:49:30 -0500

    session 8 +exercises

---
+++ 9/matplotlib.py
@@ -0,0 +1,15 @@
+#
+#
+#
+import matplotlib.pyplot as plt
+
+x = [100, 200, 400, 800, 1000]
+y = [117,483,1920,7823,12395]
+
+plt.plot(x, y)
+#plt.show()
+
+x = [20,23,25]
+y = [4,9,13]
+
+plt.scatter(x, y)
--- 9/requests.py
+++ 9/requests.py
@@ -4,6 +4,22 @@
 import requests
 
 r = requests.get("https://api.github.com/search/repositories?q=language:python")
+#vs
+#r = requests.get("https://api.github.com/users/Connor-SM")
+#vs
+#r = requests.get("https://api.github.com/users/yours")
 data = r.json( )
 
 print(type(data))
+
+print(data.keys())
+#vs
+#print(data.items())
+#vs
+#for k,v in data.items():
+#    print(k,'->',v,'\n')
+
+
+print(data['total_count'])
+#vs
+#print(data['login'])
